0be14d190e51028d932666f0a87a807c4db7466a
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-08-08 07:47:36 +0300

    Update weatherbot.py

--- weatherbot.py
+++ weatherbot.py
@@ -9,7 +9,7 @@ import json
 
 #Instead of tg_key insert your API token from BotFather
 bot = Bot(token=os.getenv('tg_key'))
-
+#Instead of weather_key insert your API token from OpenWeather
 open_weather_token = os.getenv('weather_key')
 
 dp = Dispatcher(bot)
